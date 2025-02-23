f546d6f5fbeec0cec608ea36807ed4746d51ce09
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-02-23 18:01:03 -0500

    Add item list view

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Item
 
-def index(request):
-    return HttpResponse("Hello, world!")
+
+class ItemListView(generic.ListView):
+    model = Item
+    template_name = 'catalog/item_list.html'
