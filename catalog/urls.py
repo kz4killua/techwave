6c8eb50b108dd43c428d4ebeb6d7951b04c8e308
Yannis-Munayi <167663637+Yannis-Munayi@users.noreply.github.com>
2025-02-27 14:33:45 -0500

    Can Edit item listings

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.ItemListView.as_view(), name='item_list'),
     path('create/', views.ItemCreateView.as_view(), name='item_create'),
+    path('edit/<int:item_id>/', views.edit_item, name='edit_item'),
 ]
