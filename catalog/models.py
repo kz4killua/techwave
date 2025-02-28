68084d83e49a1a55bf9996abbdffb21b547d5970
yannis-munayi <ytmunayi@gmail.com>
2025-02-28 14:40:17 -0500

    Removed Product class + items can not duplicate

--- catalog/models.py
+++ catalog/models.py
@@ -2,15 +2,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=100)
-    stock = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-    
-class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     stock = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
