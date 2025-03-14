88f6f395afc03b444074e0009353b6310a63d018
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-03-14 14:43:53 -0400

    Add minimum value to price

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Item(models.Model):
     name = models.CharField(max_length=100, unique=True)
     stock = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
 
 
     def __str__(self):
