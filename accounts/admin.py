1e7a049ad77deb7165117d472cf3089f3cb25c6b
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-03-20 21:29:02 -0400

    Add admin site for creating and deleting users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
+
 from .models import User
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "is_staff")
+    list_filter = ("is_staff",)
+    search_fields = ("username",)
+
+    fieldsets = (
+        (_("Credentials"), {"fields": ("username", "password")}),
+        (_("Permissions"),{"fields": ("is_staff",),}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
