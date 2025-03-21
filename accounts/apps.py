ffcade51845350fb4c3884ca54eecb8e55803e5e
andydomkeu <andydomkeu57@gmail.com>
2025-03-21 14:18:26 -0400

    Add admin site for creating and deleting users

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
+    # Specifies the default primary key field type for models in this app
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Defines the name of the application, which should match the app directory name
     name = 'accounts'
